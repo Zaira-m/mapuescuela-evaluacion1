2afb8871f29077ca8dc53141771e24b6a344d394
Zaira-m <zaira.manriquez@alumno.iplacex.cl>
2026-08-27 12:58:26 -0400

    Agrega interfaz web conectada a la API para EVA3

--- src/main/java/cl/mapuescuela/api/JerseyConfig.java
+++ src/main/java/cl/mapuescuela/api/JerseyConfig.java
@@ -1,6 +1,8 @@
 package cl.mapuescuela.api;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.servlet.ServletProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -8,5 +10,15 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         packages("cl.mapuescuela.api");
+
+        property(
+                ServletProperties.FILTER_FORWARD_ON_404,
+                true
+        );
+
+        property(
+                ServerProperties.WADL_FEATURE_DISABLE,
+                true
+        );
     }
 }
